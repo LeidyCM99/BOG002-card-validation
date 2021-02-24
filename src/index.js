407459c7053600b921dbf0a349a31ff36952a838
Leidy <paoalm1@hotmail.com>
2021-02-24 10:16:20 -0500

    commitnumero1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 import validator from './validator.js';
 
 console.log(validator);
+// MOSTRAR PANTALLAS
+
+function Mostrar(){
+    document.getElementById("cajavalido").style.display = "block";
+     
+  }
+
+  // OBTENER DATOS OPCION 1
+
+function ObtenerNumero(){
+  let numero = localStorage.getItem('numero');
+
+
+} 
+// OBTENER DATOS OPCION 2
+
+// var tarjeta = ""
+
+function Datos (){
+  var tarjeta = document.getElementById('InsertarNumeroTarjeta');
+   
+
+// TARJETA DATOS 
+
+const NumeroTarjeta= document.getElementById( 'NumeroT');
+NumeroTarjeta = tarjeta;
